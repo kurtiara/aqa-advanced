01e3c4ee518c93588f8be93d4dfcab053eef8747
Oleksandr_Osadchuk <karakurtiara@gmail.com>
2024-07-09 18:57:48 +0300

    Lesson4

---
+++ jsBasics/task2.js
@@ -0,0 +1,6 @@
+let name1 = "Oleksandr";
+let name2 = "Anna";
+let name1_name2 = "Хеллоу Амигас, " + name1 + " та " + name2 + "!";
+console.log (name1_name2);
+let name1_name2_template = `Хеллоу Амигас_шаблон, ${name1} та ${name2}!`;
+console.log (name1_name2_template);
