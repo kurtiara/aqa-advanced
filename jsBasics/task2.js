b919b2ad5b70c138526e67a6aa0e6f95934efd9c
Oleksandr_Osadchuk <karakurtiara@gmail.com>
2024-07-10 18:01:49 +0300

    Lesson4-re-delivery

--- jsBasics/task2.js
+++ jsBasics/task2.js
@@ -1,6 +1,6 @@
 let name1 = "Oleksandr";
 let name2 = "Anna";
-let name1_name2 = "Хеллоу Амигас, " + name1 + " та " + name2 + "!";
-console.log (name1_name2);
-let name1_name2_template = `Хеллоу Амигас_шаблон, ${name1} та ${name2}!`;
-console.log (name1_name2_template);
+let concatenatedStrings = "Хеллоу Амигас, " + name1 + " та " + name2 + "!";
+console.log (concatenatedStrings);
+let concatenatedStringsTemplate = `Хеллоу Амигас_шаблон, ${name1} та ${name2}!`;
+console.log (concatenatedStringsTemplate);
--- jsBasics/task4-3.js
+++ jsBasics/task4-3.js
@@ -1,6 +1,6 @@
 let radius = 32;
 let height = 111;
-const PI = Math.PI
-let radius_height = PI * Math.pow(radius, 2) * height;
+// const PI = Math.PI
+let radius_height = Math.PI * Math.pow(radius, 2) * height;
 let rounded_radius_height = radius_height.toFixed(2);
 console.log(rounded_radius_height);
